3a6eb3c7b3263f8a98dcaeea0607c557ee7f4e00
etridgely <5757423+etridgely@users.noreply.github.com>
2019-10-05 22:05:42 -0700

    starting on the tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,20 @@
-// Test away!
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+
+import Controls from "./Controls";
+
+test("it should render correctly", () => {
+    const toggleLocked = jest.fn();
+    const toggleClosed = jest.fn();
+
+    render(
+        <Controls
+            toggleLocked={toggleLocked}
+            toggleClosed={toggleClosed}
+            locked={locked}
+            closed={closed}
+        />
+    );
+});
+
+test("")
